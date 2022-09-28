4bf0be1c95283db64d0e1de3adb2a56611f0cc7d
frustrated-coder <d.n_mwangi@outlook.com>
2022-09-28 15:44:26 +0300

    finished assignment

--- src/index.js
+++ src/index.js
@@ -1,16 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./Home.js";
+import NavBar from "./NavBar.js";
+import About from "./About.js";
+import Login from "./Login.js";
+import SignUp from "./SignUp.js";
+/* Add basic styling for NavLinks */
 
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
 
-function App() {
-  return <Home />;
-}
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// function App() {
+//   return <Home />;
+// }
+
+ReactDOM.render(
+  <BrowserRouter>
+    <NavBar />
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route path="/signup">
+        <SignUp />
+      </Route>
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
